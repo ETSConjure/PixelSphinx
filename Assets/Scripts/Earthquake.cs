546bfa405e3a22a11172dc43d9b614acc15ac863
Sophie <soph.boucher@gmail.com>
2016-04-08 08:54:05 -0400

    started earthquake

---
+++ Assets/Scripts/Earthquake.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Earthquake : MonoBehaviour {
+    int gaugeLevel;
+    const int gaugeMax=4;
+
+	// Use this for initialization
+    void Start()
+    {
+        gaugeLevel = 0;
+        InvokeRepeating("fillGauge", 1, 1F);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void fillGauge()
+    {
+        if (gaugeLevel < gaugeMax)
+        {
+            gaugeLevel += 1;
+        }
+        else
+        {
+            gaugeLevel = 0;
+        }
+       print("gauge is at: " + gaugeLevel);
+    }
+}
