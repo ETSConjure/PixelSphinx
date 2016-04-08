742952ee79c4d22ef775fc63fb3d675fe4d79fc9
RosimInc <rosiminc7@gmail.com>
2016-04-08 02:13:34 -0400

    Added assets, input handlers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using InputHandler;
+using MenusHandler;
+
+public class GameManager : MonoBehaviour
+{
+    public int PlayerCount = 4;
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get { return _instance; }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+
+        /*
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }*/
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < PlayerCount; i++)
+        {
+            InputManager.Instance.AddCallback(i, HandleMenuInput);
+        }
+
+
+        // play gameplay music
+        //MusicManager.Instance.PlayGameplayMusic();
+    }
+
+    public void PushMenuContext()
+    {
+        for (int i = 0; i < PlayerCount; i++)
+        {
+            InputManager.Instance.PushActiveContext("Menu", i);
+        }
+    }
+
+    public void PopMenuContext()
+    {
+        for (int i = 0; i < PlayerCount; i++)
+        {
+            InputManager.Instance.PopActiveContext(i);
+        }
+    }
+
+    private void HandleMenuInput(MappedInput input)
+    {
+        float yAxis = 0f;
+
+        if (input.Ranges.ContainsKey("SelectOptionUp"))
+        {
+            yAxis = input.Ranges["SelectOptionUp"];
+        }
+        else if (input.Ranges.ContainsKey("SelectOptionDown"))
+        {
+            yAxis = -input.Ranges["SelectOptionDown"];
+        }
+
+        bool accept = input.Actions.Contains("Accept");
+
+        MenusManager.Instance.SetInputValues(accept, false, 0f, yAxis);
+    }
+}
---
+++ Assets/Scripts/Menus/SimpleMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SimpleMenu : MonoBehaviour
+{
+	public int NextLevel;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown) {
+			if (NextLevel == 200)
+				Application.Quit();
+			else
+				SceneManager.LoadScene(NextLevel);
+        }
+	}
+
+    IEnumerator AutoSkip() {
+
+        yield return new WaitForSeconds(5);
+        
+
+    }
+}
