7fbc9e1d4e6a0b0f62adebc1a049e5a733297868
Sophie <soph.boucher@gmail.com>
2016-04-08 01:25:19 -0400

    starting asteroid

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour
+{
+    Vector3 center;
+    public float speed;
+    public float step;
+
+	// Use this for initialization
+	void Start () {
+        center = new Vector3(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            MoveObject(center);
+
+	}
+
+    void MoveObject(Vector3 center)
+    {
+        step = speed * Time.deltaTime;
+        this.transform.position = Vector3.MoveTowards(transform.position, center, step);
+    }
+}
---
+++ Assets/SpawnAsteroids.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAsteroids : MonoBehaviour {
+
+    public bool doSpawn;
+
+	// Use this for initialization
+	void Start () {
+        doSpawn = true;
+        InvokeRepeating("LaunchProjectile", 0, 0.4F);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
