8b8de8b58a4a4e0b88d43b9ad2abce68070d14d0
Sophie <soph.boucher@gmail.com>
2016-04-08 01:54:33 -0400

    starting spawn asteroid

---
+++ Assets/Scripts/SpawnAsteroids.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAsteroids : MonoBehaviour {
+
+    public GameObject myAsteroid;
+    public Vector3 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", 0, 0.5F);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn()
+    {
+        GameObject instance = Instantiate(myAsteroid);
+        instance.transform.position = new Vector3(10, 10, 0);
+    }
+}
