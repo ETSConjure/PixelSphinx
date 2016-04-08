a20f6027dde3fe323f6342c214982fa96404b169
Jean-Sébastien Gervais <jsgervais@hotmail.com>
2016-04-08 19:57:39 -0400

    Random asteroids, driers and tv's.  Don't ask.

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -1,25 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Collections.Generic;
 
 public class AsteroidSpawner : TimerFunctionsClass
 {
 
     public float NextSpawnTime = 1.0f;
-    public GameObject AsteroidPrefab;
+    public GameObject AsteroidPrefab1;
+    public GameObject AsteroidPrefab2;
+    public GameObject AsteroidPrefab3;
+    public GameObject AsteroidPrefab4;
+    private List<GameObject> AsteroidPrefabTypes = new List<GameObject>();
+
     public bool GenerationVersLesjoueurs = false;  //random lorsque false;
 
 	// Use this for initialization
-	void Start ()
+	public void Start ()
 	{
 
-	    if (GenerationVersLesjoueurs) NextSpawnTime = 3 * NextSpawnTime;
+	    if (!AsteroidPrefab1 || !AsteroidPrefab2 || !AsteroidPrefab3 || !AsteroidPrefab4)
+	    {
+           Destroy(this.gameObject);
+           print("WARNING un type d'asteroide n'est pas defini dans les prefab. Vérifier l'objet avec un component AsteroidSpawner");
+	        return;
+	    }
+        AsteroidPrefabTypes.Add(AsteroidPrefab1);
+        AsteroidPrefabTypes.Add(AsteroidPrefab2);
+        AsteroidPrefabTypes.Add(AsteroidPrefab3);
+        AsteroidPrefabTypes.Add(AsteroidPrefab4);
+
+
+        if (GenerationVersLesjoueurs) NextSpawnTime = 3 * NextSpawnTime;
         this.SetTimer(NextSpawnTime, SpawnAsteroidEvent);
         this.StartTimer();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    public void Update () {
         base.Update();
     }
 
@@ -31,8 +49,12 @@ public class AsteroidSpawner : TimerFunctionsClass
             var x = UnityEngine.Random.Range(10.0f, 20.0f)*(Mathf.Floor(UnityEngine.Random.Range(0.0f, 1.99f))*2 - 1);
             var y = UnityEngine.Random.Range(10.0f, 20.0f)*(Mathf.Floor(UnityEngine.Random.Range(0.0f, 1.99f))*2 - 1);
 
+
+            //0-3
+            var AsteroidType = Mathf.RoundToInt(Mathf.Floor(UnityEngine.Random.Range(0f, 3.999f)));
+
             //instantiate as child of AsteroidSpawner
-            var a = Instantiate(AsteroidPrefab, new Vector3(x, y, 0.0f), Quaternion.identity);
+            var a = Instantiate(AsteroidPrefabTypes[AsteroidType], new Vector3(x, y, 0.0f), Quaternion.identity);
             //a.tranform.parent = this.transform;
 
         }
@@ -45,7 +67,12 @@ public class AsteroidSpawner : TimerFunctionsClass
                 var playerTheta = Mathf.Atan2(p.transform.position.y, p.transform.position.x); 
                 var angle = ( 360.0f + (((playerTheta * 180)) / Mathf.PI)) % 360;  ///TODO : a changer pour p.theta
                 print("angle:" + angle);
-                Instantiate(AsteroidPrefab, planet.GetPlanetCoordinatesFromPlayerXY(angle, UnityEngine.Random.Range(10f,15f)), Quaternion.identity);
+
+                var AsteroidType = Mathf.RoundToInt(Mathf.Floor(UnityEngine.Random.Range(0f, 3.999f)));
+
+                Instantiate(AsteroidPrefabTypes[AsteroidType], 
+                            planet.GetPlanetCoordinatesFromPlayerXY(angle, UnityEngine.Random.Range(10f,15f)), 
+                            Quaternion.identity);
             }
 
         }
