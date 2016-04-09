f966bf235c1015ccfb41ad332203c03d9df5a7cc
Sophie <soph.boucher@gmail.com>
2016-04-09 16:33:57 -0400

    WorldManager singelton

---
+++ Assets/Scripts/WorldManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+
+    private static WorldManager instance = null; 
+    private WorldManager(){}             
+
+	// Use this for initialization
+	public void Awake () {
+        if (!instance)
+        {
+            instance = new WorldManager();
+        }
+	}
+
+    public WorldManager getInstance() {
+        return instance;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
